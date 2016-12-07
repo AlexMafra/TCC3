1cc2904878948a8efc18581c97a71d1f5278dfdc
revieira <renan.vieira16@gmail.com>
2016-12-07 19:44:48 -0200

    7 DE NOVEMBRO

---
+++ clienteMenu1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="boots.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<li><a class="navbar-brand">
+						<!--<img alt="Thompson & Vasconcellos" src="LogoTCC2.jpg">-->
+					</a></li>
+				</div>
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="sectionAgenda.php">INICIO</a></li>
+					<li><a href="clienteDados.php">CADASTRO</a></li>
+					<li class="active"><a href="sectionAgenda.php">AGENDA</a></li>
+					<li><a href="clienteProc.php">PROCESSOS</a></li>
+				</ul>
+			</div>
+</nav>
+
+</body>
+</html>
